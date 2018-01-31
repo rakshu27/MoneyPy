6412e3ad6f9f4c9a0a523eea91bdd4e6a0ba3231
Fabian <fh914@nyu.edu>
2018-01-31 13:19:32 -0500

    Initial Tox and setup.py setup
    Added the tox.ini file, with some basic information for testing in PY36 env.  Added basic setup.py info to get tox running.

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
- 
+import setuptools
+
+setuptools.setup(
+    name="MoneyPy",
+    version="0.0",
+    url="https://github.com/learn-and-code/MoneyPy",
+
+    author="learnandcode.it",
+
+    description="A python framework to build custom budget-analysis workflows and applications",
+
+    packages=setuptools.find_packages(),
+
+    test_suite='tests',
+
+    license='MIT',
+
+    install_requires=[],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.6'
+    ],
+) 
