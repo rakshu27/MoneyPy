8813367b209c9cd7a4892283edb4fc19e0cc981c
Arne Küderle <a.kuederle@gmail.com>
2018-02-25 19:48:36 +0100

    Some pep8

--- tests/exporters/test_database.py
+++ tests/exporters/test_database.py
@@ -1,46 +1,50 @@
+import os
 import unittest
+from datetime import date
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, exc
+
 from moneypy.exporters.models import Base, ExpenseDetails, ExpenseLabels
-from datetime import date, datetime
-import os
- 
-class DatabaseTesting(unittest.TestCase):
-    sampleRecords=[{
-        'id':'23',
-        'amount':500,
-        'date':date(2017, 10, 17),
-        'recipient':'Aircel',
-        'description':'Aircel Prepaid Bill',
-        'labels':['cellular', 'Entertainment']
+
+
+class TestDatabase(unittest.TestCase):
+    sampleRecords = [{
+        'id': '23',
+        'amount': 500,
+        'date': date(2017, 10, 17),
+        'recipient': 'Aircel',
+        'description': 'Aircel Prepaid Bill',
+        'labels': ['cellular', 'Entertainment']
     },
-    {
-        'id':'25',
-        'amount':1000,
-        'date':date(2017, 4, 11),
-        'recipient':'Amazon',
-        'description':'Python Book',
-        'labels':['Education', 'Online shopping']
-    },
-    {
-        'id':'27',
-        'amount':2000,
-        'date':date(2017, 11, 12),
-        'recipient':'Netflix',
-        'description':'Account Subscription to watch movies',
-        'labels':['Entertainment']
-    },
-    {
-        'id':'29',
-        'amount':2000,
-        'date':date(2017, 12, 22),
-        'recipient':'Amazon',
-        'description': 'Bought new dresses',
-        'labels':['Entertainment','Online shopping']
-    }]
-    labelCount=0
+        {
+            'id': '25',
+            'amount': 1000,
+            'date': date(2017, 4, 11),
+            'recipient': 'Amazon',
+            'description': 'Python Book',
+            'labels': ['Education', 'Online shopping']
+        },
+        {
+            'id': '27',
+            'amount': 2000,
+            'date': date(2017, 11, 12),
+            'recipient': 'Netflix',
+            'description': 'Account Subscription to watch movies',
+            'labels': ['Entertainment']
+        },
+        {
+            'id': '29',
+            'amount': 2000,
+            'date': date(2017, 12, 22),
+            'recipient': 'Amazon',
+            'description': 'Bought new dresses',
+            'labels': ['Entertainment', 'Online shopping']
+        }]
+    labelCount = 0
+
     def populateSampleData(self):
-        for record in DatabaseTesting.sampleRecords:
+        for record in self.sampleRecords:
             expenseid = record['id']
             amount = record['amount']
             recipient = record['recipient']
@@ -48,8 +52,10 @@ class DatabaseTesting(unittest.TestCase):
             expenseDate = record['date']
             labels = record['labels']
             labelInstances = self.getLabelInstances(labels)
-            expenseObject = ExpenseDetails(id=expenseid, date=expenseDate, amount=amount, recipient=recipient, description=description, labels=labelInstances)
+            expenseObject = ExpenseDetails(id=expenseid, date=expenseDate, amount=amount, recipient=recipient,
+                                           description=description, labels=labelInstances)
             self.session.add(expenseObject)
+
     def getLabelInstances(self, labels):
         labelInstances = []
         for label in labels:
@@ -57,37 +63,44 @@ class DatabaseTesting(unittest.TestCase):
                 labelInstance = self.session.query(ExpenseLabels).filter_by(label=label).one()
                 labelInstances.append(labelInstance)
             except exc.NoResultFound:
-                labelId = 'Label'+str(DatabaseTesting.labelCount)
+                labelId = 'Label' + str(self.labelCount)
                 labelInstance = ExpenseLabels(id=labelId, label=label)
-                DatabaseTesting.labelCount += 1
+                self.labelCount += 1
                 labelInstances.append(labelInstance)
             except exc.MultipleResultsFound:
                 raise
         return labelInstances
+
     def setUp(self):
-        engine = create_engine('sqlite:///data/test_money_manager.db', echo=False)
+        engine = create_engine('sqlite:///tmp/test_money_manager.db', echo=True)
         self.engine = engine
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         self.session = Session()
         self.populateSampleData()
         self.session.commit()
+
     def tearDown(self):
         self.session.close()
         Base.metadata.drop_all(self.engine)
         try:
-            filename= '.\\data\\test_money_manager.db'
+            filename = '.\\data\\test_money_manager.db'
             os.remove(filename)
         except FileNotFoundError:
             pass
+
     def test_recordInsertionInExpenseDetailsTableByCheckingCount(self):
         count = self.session.query(ExpenseDetails).count()
-        self.assertEqual(count, len(DatabaseTesting.sampleRecords), 'Given records are not inserted properly in ExpenseDetails table')
+        self.assertEqual(count, len(self.sampleRecords),
+                         'Given records are not inserted properly in ExpenseDetails table')
+
     def test_backPopulationInExpenseLabelsTableByCheckingCount(self):
         count = self.session.query(ExpenseLabels).count()
-        self.assertEqual(count, DatabaseTesting.labelCount, 'Labels are not back populated correctly in ExpenseLabels Table') 
+        self.assertEqual(count, self.labelCount,
+                         'Labels are not back populated correctly in ExpenseLabels Table')
+
     def test_filterLabelsByExpenseId(self):
-        record = DatabaseTesting.sampleRecords[3]
+        record = self.sampleRecords[3]
         recordId = record['id']
         expectedLabels = record['labels']
         actualLabels = []
@@ -100,9 +113,10 @@ class DatabaseTesting(unittest.TestCase):
             raise
         except exc.MultipleResultsFound:
             raise
+
     def test_filterExpensesByLabel(self):
         sampleLabel = 'Entertainment'
-        expectedExpenseIds = ['23','27','29']
+        expectedExpenseIds = ['23', '27', '29']
         actualExpenseIds = []
         try:
             labelInstance = self.session.query(ExpenseLabels).filter_by(label=sampleLabel).one()
@@ -113,10 +127,3 @@ class DatabaseTesting(unittest.TestCase):
             raise
         except exc.MultipleResultsFound:
             raise
-        
-
-    
-
-
-
-    
